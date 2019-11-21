b8b064bf97a0e0aecfbc666bb37c551d4801dfb2
Brian Yeager <brianyeager@Brians-MacBook-Pro.local>
2019-11-21 16:13:11 -0500

    Changed name

--- src/app.js
+++ src/app.js
@@ -89,7 +89,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Brian Yeager',
         errorMessage: 'Help article not found'
     })
 })
@@ -97,7 +97,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name:   'Andrew Mead',
+        name:   'Brian Yeager',
         errorMessage: 'Page not found'
     })
 })
